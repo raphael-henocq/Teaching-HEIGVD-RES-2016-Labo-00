f70dc1de05290582e10c99600c89e68a6bed2ae7
Henocq Raphael <raphael.henocq@heig-vd.ch>
2016-02-24 16:15:13 +0000

    Labo 1 Henocq Raphael

--- Lab00App-build/Lab00App-code/src/main/java/ch/heigvd/res/lab00/Flute.java
+++ Lab00App-build/Lab00App-code/src/main/java/ch/heigvd/res/lab00/Flute.java
@@ -1,14 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+//Author : Henocq Raphael
 package ch.heigvd.res.lab00;
 
-/**
- *
- * @author Raphael
- */
+
 public class Flute implements IInstrument {
    public String play() {return "bip";}
    public String getColor() {return "White";}
---
+++ Lab00App-build/Lab00App-code/src/main/java/ch/heigvd/res/lab00/IInstrument.java
@@ -0,0 +1,9 @@
+//Author : Henocq Raphael
+package ch.heigvd.res.lab00;
+
+
+interface IInstrument {
+   public String play();
+   public int getSoundVolume();
+   public String getColor();
+}
