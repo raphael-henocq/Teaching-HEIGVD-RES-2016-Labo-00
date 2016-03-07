ea7ab1505a9f7dd3169f794ca382438a2106b240
Henocq Raphael <raphael.henocq@heig-vd.ch>
2016-03-07 22:31:36 +0000

    Orchestra feature implemented

--- Lab00App-build/Lab00App-code/src/main/java/ch/heigvd/res/lab00/Flute.java
+++ Lab00App-build/Lab00App-code/src/main/java/ch/heigvd/res/lab00/Flute.java
@@ -3,7 +3,7 @@ package ch.heigvd.res.lab00;
 
 
 public class Flute implements IInstrument {
-   public String play() {return "bip";}
+   public String play() {return "trilili";}
    public String getColor() {return "White";}
    public int getSoundVolume(){return 5;}
 }
